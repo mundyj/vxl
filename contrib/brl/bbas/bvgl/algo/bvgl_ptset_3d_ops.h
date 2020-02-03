a11377dd07dd27614e2bf35b75f1e20fe26bd7f4
Joseph Mundy <jlm@visionsystemsinc.com>
2020-02-03 07:02:24 -0500

    added ptset transforms and bpgl_geotif init_from_dsm

--- contrib/brl/bbas/bvgl/algo/bvgl_ptset_3d_ops.h
+++ contrib/brl/bbas/bvgl/algo/bvgl_ptset_3d_ops.h
@@ -5,13 +5,14 @@
 
 //:
 // \file
-// \brief operations on vgl_pointset_x
+// \brief operations on vgl_pointset_3d
 //
 // \author J.L. Mundy
 // \date Sept. 13, 2019
 //
 // \verbatim
-//  Modifications:None
+//  January 29, 2020
+//  added noise filtering algorithm
 // \endverbatim
 
 
@@ -21,9 +22,11 @@
 #  include <vcl_msvc_warnings.h>
 #endif
 #include <vnl/vnl_math.h>
+#include <vnl/vnl_matrix_fixed.h>
+#include <vector>
 #include <vgl/vgl_pointset_3d.h>
 #include <bvgl/bvgl_k_nearest_neighbors_3d.h>
-
+#include <vgl/algo/vgl_rotation_3d.h>
 template <class T>
 class bvgl_ptset_3d_ops
 {
@@ -38,13 +41,41 @@ public:
     ptset_ = ptset;
     knn_ = bvgl_k_nearest_neighbors_3d<T>(ptset_);
  }
+ //: find average distance between neighboring points
+ //  used to define a tolerance on distance
+ //  for speed only a random fraction of the points is
+ //  used to compute average distance
+ T average_distance(T frac_ptset = T(1));
+
+ //: roughness value is added as a scalar property of each point
+ // roughness is standard deviation of neighboring points from a fitted plane
+ // radius determines the set of points (a subset of neighbors with d<radius)
  void compute_roughness(T radius, size_t n_nbrs);
+
+ //: remove points that are outliers to a local planar fit
+ void remove_noise(T radius, T dist_tol, size_t n_nbrs);
+
+ //: find the rotation to diagonalize the covariance matrix of the pointset
+ static bool R_to_diagonalize_covar(vgl_pointset_3d<T> const& ptset, vnl_matrix_fixed<T,3,3>& R,
+                                    std::vector<T>& lambda,  T frac_ptset = T(1));
+ 
+ //: rotate a pointset with a 3x3 matrix 
+ static vgl_pointset_3d<T> Rtrans(vgl_pointset_3d<T> const& ptset, vnl_matrix_fixed<T,3,3> const& R);
+
+ //: transform a pointset with a similarity transform (translate_first = true means translate then scale and rotate)
+ // translate_first = false means rotate and scale then translate
+ static vgl_pointset_3d<T> Strans(vgl_pointset_3d<T> const& ptset, T scale, vnl_matrix_fixed<T,3,3> const& R,
+                                  vnl_vector_fixed<T,3> const& t, bool translate_first = true);
+
  const vgl_pointset_3d<T>& scalar_result() const {return ptset_with_scalar_result_;}
  const bvgl_k_nearest_neighbors_3d<T>& knn(){return knn_;}
+ const vgl_pointset_3d<T>& result(){return ptset_result_;}
+
+
  private:
-  size_t n_nbrs_;
   vgl_pointset_3d<T> ptset_;
-  vgl_pointset_3d<T> ptset_with_scalar_result_;
+  vgl_pointset_3d<T> ptset_result_;
+  vgl_pointset_3d<T> ptset_with_scalar_result_;//e.g. scalar is roughness
   bvgl_k_nearest_neighbors_3d<T> knn_;
 };
 
