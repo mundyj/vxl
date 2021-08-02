a7b8a99d76fe2d400d2dfb9fa82d4abeeec24fee
Joseph Mundy <jlm@visionsystemsinc.com>
2021-08-02 15:54:40 -0400

    2nd generation code

--- contrib/brl/bseg/bsgm/bsgm_error_checking.hxx
+++ contrib/brl/bseg/bsgm/bsgm_error_checking.hxx
@@ -50,163 +50,112 @@ void bsgm_check_shadows(
         disp_img(x, y) = invalid_disparity;
       }
     }
-  }
+  }     
 }
-template <class T>
-void
-bsgm_remove_shadow_overhang(vil_image_view<float> & disp_img,
-                            const vil_image_view<T> & img_tar,
-                            const vil_image_view<T> & img_ref,
-                            const vgl_vector_2d<float> & sun_dir,
-                            int shadow_high,
-                            int shadow_low,
-                            float shadow_gradient_thresh,
-                            float invalid_disparity)
-{
-  // compute scan pixels
-  vgl_point_2d<float> p0(0.0f, 0.0f), pp, pm;
-  float rad = 4.0f;
-  pm = p0 + rad * sun_dir;
-  pp = p0 - 2.0f*rad * sun_dir;
-  float xs = pm.x(), ys = pm.y();
-  float xe = pp.x(), ye = pp.y();
-  bool init = true;
-  int ir = int(rad);
-  int start = -ir;
-  float x, y;
-  std::vector<std::tuple<int, int, int> > deriv_pix_offset;
-  std::vector<std::pair<int, int> > step_map(9);
+/*
+                             *                          
+                          *  |
+                       *     |
+       radius       *    +   |
+    ------------ * ------------ k  the filter kernel is a 1st derivative operator
+    |  -      *      radius
+    |      *
+    |   *
+    |*
 
+  The filter is applied in the opposite sense to the sun dir unit vector
+*/
+static std::vector<std::tuple<int, int, int> > step_filter(int radius, vgl_vector_2d<float> const& sun_dir, int& sum_coef){
+  vgl_point_2d<float> p0(0.0f, 0.0f), pp, pm;
+  float margin = 3.0f;
+  float mag = margin*radius;
+  // generate a pixel quantized vector opposite to sun_dir
+  pm = p0 + mag * sun_dir;
+  pp = p0 - mag * sun_dir;
+  float xs = pm.x(), ys = pm.y(); // start point
+  float xe = pp.x(), ye = pp.y(); // end point
+  bool init = true;
+  int start = -int(mag)
+  float x, y;
+  std::vector<std::tuple<int, int, int> > ret;
+  sum_coef = 0;
   while (brip_line_generator::generate(init, xs, ys, xe, ye, x, y))
   {
-    if(start == rad+1)
-      break;
-    deriv_pix_offset.push_back(std::tuple<int, int, int>(int(x), int(y), start++));
+    if (start>=(-radius) && start<=(radius)){
+      deriv_pix_offset.push_back(std::tuple<int, int, int>(int(x), int(y), start));
+      sum_coef+=start;
+    }
+    start++;
   }
-  int ni = img_tar.ni(), nj = img_tar.nj(), ns = deriv_pix_offset.size();
-  int sumd = 0, sumc = 0;
-  for (int k = 0; k < ns; ++k)
-  {
-    int w = std::get<2>(deriv_pix_offset[k]);
-    // std::cout << k << ' ' << w << std::endl;
-    sumd += abs(w);
+  return ret;
+}  
+template <class T>
+void bsgm_shadow_step_filter(
+  const vil_image_view<T>& img,
+  const vil_image_view<T>& step_img,
+  const vgl_vector_2d<float>& sun_dir,
+  int response_low,
+  int shadow_high){
+  int radius = 3;
+  int sum_coef;
+  std::vector<std::tuple<int, int, int> > deriv_pix_offset = step_filter(radius, sun_dir, sum_coef);
 
-    if (k == 0)
-    {
-      int di = std::get<0>(deriv_pix_offset[2]);
-      int dj = std::get<1>(deriv_pix_offset[2]);
-      step_map[k] = std::pair<int, int>(di, dj);
-    }
-    else if (k > 0 && k <= 3)
-    {
-      int di = std::get<0>(deriv_pix_offset[3]);
-      int dj = std::get<1>(deriv_pix_offset[3]);
-      step_map[k] = std::pair<int, int>(di, dj);
-    }
-    else if (k == 4)
-    {
-      int di = std::get<0>(deriv_pix_offset[4]);
-      int dj = std::get<1>(deriv_pix_offset[4]);
-      step_map[k] = std::pair<int, int>(di, dj);
-    }
-    else if (k > 4 && k <= 7)
-    {
-      int di = std::get<0>(deriv_pix_offset[5]);
-      int dj = std::get<1>(deriv_pix_offset[5]);
-      step_map[k] = std::pair<int, int>(di, dj);
-    }
-    else if (k == 8)
-    {
-      int di = std::get<0>(deriv_pix_offset[6]);
-      int dj = std::get<1>(deriv_pix_offset[6]);
-      step_map[k] = std::pair<int, int>(di, dj);
-    }
-  }
-  
-  vil_image_view<float> sun_dscan(ni, nj);
-  vil_image_view<float> sun_peak(ni, nj);
-  vil_image_view<T> mapped_target = img_tar;
-  // vil_image_view<float> sun_cscan(ni, nj);
-  sun_dscan.fill(0.0f);
-  sun_peak.fill(0.0f);
+  int ni = img.ni(), nj = img.nj();
+  vil_image_view<int> resp_img(ni, nj);
+  resp_img.fill(0);
+  vil_image_view<T> center(ni, nj);
+  center.fill(0);
+  vil_image_view<T>& step_img(ni, nj);
+  step_img.fill(0);
 
+  // border
   int itstart = int(rad) + 1;
   for (int j = itstart; j < (nj - itstart); ++j)
     for (int i = itstart; i < (ni - itstart); ++i)
     {
-      float dsum = 0.0f, csum = 0.0;
-      int vmin = 4000.0f;
+      int resp = 0;
+      int vmin = 2048; //max 11 bits + 1
       for (int k = 0; k < ns; ++k)
       {
         int di = std::get<0>(deriv_pix_offset[k]);
         int dj = std::get<1>(deriv_pix_offset[k]);
         int wd = std::get<2>(deriv_pix_offset[k]);
-        float v = img_tar(i + di, j + dj);
-        dsum += wd * v;
+        float v = img(i + di, j + dj);
+        resp += wd * v;
         if(v<vmin)
           vmin = v;
       }
-      dsum /= sumd;
-      if(vmin < 200.0f&&dsum>200.0f)
-        sun_dscan(i, j) = dsum;
+      resp /= sum_coef;
+      
+      if(vmin < shadow_high && resp > response_low)
+        resp_img(i, j) = resp;
     }
-  bool save_result = true;
-  int c = int(rad);
-  // compute center
+  // compute peak response location
+  T max_pix = std::numeric_limits<T>::max();
   for (int j = itstart; j < (nj - itstart); ++j)
     for (int i = itstart; i < (ni - itstart); ++i)
     {
-      float v = sun_dscan(i, j);
-      if(v == 0.0f)
+      int v = resp_img(i, j);
+      if(v == 0)
         continue;
       int max_k = 0;
-      float max_v = 0.0f;
+      int max_v = 0.0f;
       for (int k = 0; k < ns; ++k)
       {
-        int di = step_map[k].first;
-        int dj = step_map[k].second;
-        float v = sun_dscan(i + di, j + dj);
+        int di = deriv_pix_offset[k].first;
+        int dj = deriv_pix_offset[k].second;
+        float v = resp_img(i + di, j + dj);
         if(v>max_v){
           max_v = v;
           max_k = k;
         }
-        if(max_k == c)
-          sun_peak(i, j) = max_v;
+      }
+      if(max_k == radius||max_k == radius-1||max_k == radius+1){
+        if(max_v > max_pix)
+          max_v = max_pix;
+        center(i, j) = max_v;
       }
     }
-  // compress step
-  for (int j = itstart; j < (nj - itstart); ++j)
-    for (int i = itstart; i < (ni - itstart); ++i)
-    {
-      float v = sun_peak(i, j);
-      if(v == 0.0f)
-        continue;
-      int max_k = 0;
-      float max_v = 0.0f;
-      for (int k = 0; k < ns; ++k)
-      {
-        int si = step_map[k].first;
-        int sj = step_map[k].second;
-        T v = img_tar(i + si, j + sj);
-        int di = std::get<0>(deriv_pix_offset[k]);
-        int dj = std::get<1>(deriv_pix_offset[k]);
-        int im = i + di, jm = j + dj;
-        if (im < 0 || im >= ni || jm < 0 || jm >= nj)
-          continue;
-        mapped_target(i+di, j+dj) = v;
-      }
-    }
-      
-  if(save_result){
-    std::string dpath = "D:/tests/BuckleyAFB/test_results/sun_dscan_tar.tif";
-    std::string ddisp_path = "D:/tests/BuckleyAFB/test_results/disp_debug.tif";
-    std::string peak_path = "D:/tests/BuckleyAFB/test_results/peak_resp.tif";
-    std::string mapped_target_path = "D:/tests/BuckleyAFB/test_results/mapped_target.tif";
-    vil_save(sun_dscan, dpath.c_str());
-    vil_save(disp_img, ddisp_path.c_str());
-    vil_save(sun_peak, peak_path.c_str());
-    vil_save(mapped_target, mapped_target_path.c_str());
-  }
 }
 template <class T>
 void bsgm_compress_shadow_step_response(
@@ -240,6 +189,7 @@ void bsgm_compress_shadow_step_response(
     start++;
   }
   int ni = img_tar.ni(), nj = img_tar.nj(), ns = deriv_pix_offset.size();
+  std::vector<std::tuple<int, int, int, int, int, int, int> > steps;
   int sumd = 0, sumc = 0;
   for (int k = 0; k < ns; ++k)
   {
@@ -248,41 +198,22 @@ void bsgm_compress_shadow_step_response(
     sumd += abs(w);
     int ti = std::get<0>(deriv_pix_offset[k]);
     int tj = std::get<1>(deriv_pix_offset[k]);
-    T v = img_tar(511 + ti, 1081 + tj);//ref (500, 1081)
-    std::cout << ti << ' ' << tj << ' ' << v <<  std::endl;
-
-    if (k == 0)
-    {
-      int di = std::get<0>(deriv_pix_offset[2]);
-      int dj = std::get<1>(deriv_pix_offset[2]);
-      step_map[k] = std::pair<int, int>(di, dj);
-    }
-    else if (k > 0 && k <= 3)
-    {
-      int di = std::get<0>(deriv_pix_offset[3]);
-      int dj = std::get<1>(deriv_pix_offset[3]);
-      step_map[k] = std::pair<int, int>(di, dj);
-    }
-    else if (k == 4)
-    {
-      int di = std::get<0>(deriv_pix_offset[4]);
-      int dj = std::get<1>(deriv_pix_offset[4]);
-      step_map[k] = std::pair<int, int>(di, dj);
-    }
-    else if (k > 4 && k <= 7)
-    {
-      int di = std::get<0>(deriv_pix_offset[5]);
-      int dj = std::get<1>(deriv_pix_offset[5]);
-      step_map[k] = std::pair<int, int>(di, dj);
-    }
-    else if (k == 8)
-    {
-      int di = std::get<0>(deriv_pix_offset[6]);
-      int dj = std::get<1>(deriv_pix_offset[6]);
-      step_map[k] = std::pair<int, int>(di, dj);
-    }
+    T v0 = img_tar(511 + ti, 1081 + tj);//ref (500, 1081)
+    T v1 = img_tar(514 + ti, 1097 + tj);
+    T v2 = img_tar(507 + ti, 1065 + tj);
+    T v3 = img_tar(345 + ti, 642 + tj);
+    T v4 = img_tar(347 + ti, 649 + tj);
+    T v5 = img_tar(704 + ti, 550 + tj);
+    T v6 = img_tar(707 + ti, 563 + tj);
+    steps.emplace_back(v0, v1, v2, v3, v4, v5, v6);
+    std::cout << ti << ' ' << tj << ' ' << v0 << ' ' << v1 << ' ' << v2 << ' '<< v3 << ' ' << v4 << ' '<< v5 << ' ' << v6 << std::endl;
   }
-  
+  for (int k = 0; k < ns; ++k)
+  {
+    if (k < 3) std::cout << std::get<0>(steps[0]) << ' ' << std::get<3>(steps[0]) << ' ' << std::get<5>(steps[0]) << std::endl;
+    if (k == 3) std::cout << std::get<0>(steps[0]) << ' ' << std::get<3>(steps[0]) << ' ' << std::get<5>(steps[0]) << std::endl;
+    if (k > 3) std::cout << std::get<0>(steps[6]) << ' ' << std::get<3>(steps[6]) << ' ' << std::get<5>(steps[6]) << std::endl;
+  }
   vil_image_view<float> sun_dscan_tar(ni, nj);
   vil_image_view<float> sun_peak_tar(ni, nj);
   vil_image_view<T> mapped_target = img_tar;
@@ -744,9 +675,8 @@ template void bsgm_compute_invalid_map(const vil_image_view<T>& , const vil_imag
 template void bsgm_check_nonunique(vil_image_view<float>& , const vil_image_view<unsigned short>&,\
                                    const vil_image_view<T>&, float, unsigned short, int,          \
                                    const vgl_box_2d<int>&);                                       \
-template void bsgm_remove_shadow_overhang(vil_image_view<float>&, const vil_image_view<T>&,       \
-                                          const vil_image_view<T>&, const vgl_vector_2d<float>&,  \
-                                          int, int, float, float);                                \
+template void bsgm_shadow_step_filter(const vil_image_view<T>&,const vil_image_view<T>&,          \
+                                      const vgl_vector_2d<float>&, int, int )                     \
 template void bsgm_compress_shadow_step_response(vil_image_view<T>&, vil_image_view<T>&,          \
                                                  const vgl_vector_2d<float>&,                     \
                                                   const vgl_vector_2d<float>&,                    \
