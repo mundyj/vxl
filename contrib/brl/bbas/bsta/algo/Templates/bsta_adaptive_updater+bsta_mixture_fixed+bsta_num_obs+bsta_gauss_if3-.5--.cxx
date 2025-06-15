91f222e82638702c6191c9d42de09ab5fb3d63b4
Joseph Mundy <jlm@visionsystemsinc.com>
2025-06-15 17:22:24 -0400

    4 and 5 component mixtures

---
+++ contrib/brl/bbas/bsta/algo/Templates/bsta_adaptive_updater+bsta_mixture_fixed+bsta_num_obs+bsta_gauss_if3-.5--.cxx
@@ -0,0 +1,6 @@
+#include <bsta/algo/bsta_adaptive_updater.hxx>
+#include <bsta/bsta_gauss_if3.h>
+
+typedef bsta_mixture_fixed<bsta_num_obs<bsta_gauss_if3>,5> mix_fix_gauss_if3_5;
+
+BSTA_ADAPTIVE_UPDATER_INSTANTIATE(mix_fix_gauss_if3_5);
